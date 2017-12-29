cafaa330d6d7e830cc4864ff7d4630ef5bf3f14f
mryyomutga <mryyomutga@gmail.com>
2017-12-29 18:32:32 +0900

    create MainActivity layout

--- app/src/main/java/jp/ac/kanazawait/onewaytwitter/MainActivity.java
+++ app/src/main/java/jp/ac/kanazawait/onewaytwitter/MainActivity.java
@@ -3,22 +3,45 @@ package jp.ac.kanazawait.onewaytwitter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.content.Intent;
-import android.view.Menu;
-import android.os.Bundle;
+import android.view.*;
+import android.view.View.*;
+import android.widget.Button;
 
-import twitter4j.Twitter;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Twitterアカウントの認証
         if(!TwitterUtils.hasAccessToken(this)){
             Intent intent = new Intent(getApplicationContext(), OAuthActivity.class);
             startActivity(intent);
             finish();
         }
+
+        findViewById(R.id.tweetButton).setOnClickListener(this);
+        findViewById(R.id.authorization).setOnClickListener(this);
     }
+
+    @Override
+    public void onClick(View v){
+        if(v != null){
+            switch (v.getId()){
+                // Click Tweet Button
+                case R.id.tweetButton:
+
+                    break;
+                // Click Authorize Button
+                case R.id.authorization:
+                    Intent intent = new Intent(getApplication(), OAuthActivity.class);
+                    startActivity(intent);
+                    finish();
+                default:
+                    break;
+            }
+        }
+    }
+
 }
